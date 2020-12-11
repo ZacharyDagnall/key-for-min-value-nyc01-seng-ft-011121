6119c0be1c44a00ec4c2f3d018c4b4c4453a6142
Zachary R Dagnall <zachary.dagnall@gmail.com>
2020-12-11 14:33:21 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(name_hash)
   if name_hash.empty? 
     return NIL 
   end
-  minkey = :k
-  minval = 100000000000000000000
+  minkey = :k     #arbitrary initialization
+  minval = 100000000000000000000  #this makes me nervous because i don't know the max int available in ruby, so this could fail
   name_hash.each do |k,v|
     if v < minval
       minval = v
